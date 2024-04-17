02a85f28f416232a27b7522a15fff08cac1ec3e3
allegis-one-north <nvillapiano@onenorth.com>
2024-04-17 15:25:16 -0500

    Added Headshot component

---
+++ src/components/Headshot.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  // sample prop set-up
+  // msg: {
+  //   type: String,
+  //   required: true
+  // }
+})
+</script>
+
+<template>
+  <div class="Headshot">
+    <img src="@/assets/js-headshot.jpeg" alt="Professional headshot for Joanna Solotaroff">
+  </div>
+</template>
+
+
+
+<style scoped>
+
+.Headshot {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  
+  img {
+    display: block;
+    border-radius: 50%;
+    max-block-size: 70vh;
+    min-block-size: 50vw;
+    aspect-ratio: 1/1;
+  }
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<!-- <script setup>
-import Masthead from '../components/Masthead.vue'
-</script> -->
+<script setup>
+import Headshot from '../components/Headshot.vue'
+</script>
 
 <template>
   <main>
-    Homepage content
+    <h1>Homepage content</h1>
+    <section>
+      <Headshot></Headshot>
+    </section>
   </main>
 </template>
